172a32d736ac0b49ae141418809f896e4e7ba63e
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-10-30 22:34:46 +0700

    Initial commit and added product dependencies

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'views/screens/home.dart';
+
+Future<void> main() async {
+  await dotenv.load();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CryptoHub',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: "/",
+      routes: {
+        HomePage.routeName: (context) => HomePage(),
+      },
+    );
+  }
+}
