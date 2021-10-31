33f6ba2afabd51db13d5e71eb53ca5b8c2592732
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-10-31 11:40:15 +0700

    Add getIt service locator in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'getit.dart';
 import 'views/screens/home.dart';
 
 Future<void> main() async {
   await dotenv.load();
+  setup();
   runApp(const MyApp());
 }
 
