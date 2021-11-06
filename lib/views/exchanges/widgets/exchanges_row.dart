39e55e21fe74628709c380f11e11f676ea8eee1f
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-11-06 11:52:46 +0700

    Add on click animation to exchanges row

--- lib/views/exchanges/widgets/exchanges_row.dart
+++ lib/views/exchanges/widgets/exchanges_row.dart
@@ -36,7 +36,7 @@ class _ExchangesRowState extends State<ExchangesRow> {
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 50),
       curve: Curves.easeIn,
       child: Column(
         children: [
@@ -125,40 +125,39 @@ class _ExchangesRowState extends State<ExchangesRow> {
             ),
           ),
           AnimatedContainer(
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeIn,
-            child: _isClicked
-                ? Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      border: Border.symmetric(
-                        vertical: BorderSide(
-                          color: Colors.grey,
-                          width: 0.4,
-                        ),
-                        horizontal: BorderSide(
-                          color: Colors.grey,
-                          width: 0.6,
-                        ),
-                      ),
-                    ),
-                    child: Html(
-                      data: exchanges.description ?? "",
-                      onLinkTap: (url, _, __, ___) async {
-                        if (url != null && await canLaunch(url)) {
-                          await launch(
-                            url,
-                            enableJavaScript: true,
-                          );
-                        }
-                      },
-                    ),
-                  )
-                : Container(),
+            duration: const Duration(milliseconds: 600),
+            curve: Curves.ease,
+            constraints: BoxConstraints(
+              maxHeight: _isClicked ? 2000 : 0,
+            ),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 15,
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              border: Border.symmetric(
+                vertical: BorderSide(
+                  color: Colors.grey,
+                  width: 0.4,
+                ),
+                horizontal: BorderSide(
+                  color: Colors.grey,
+                  width: 0.6,
+                ),
+              ),
+            ),
+            child: Html(
+              data: exchanges.description ?? "",
+              onLinkTap: (url, _, __, ___) async {
+                if (url != null && await canLaunch(url)) {
+                  await launch(
+                    url,
+                    enableJavaScript: true,
+                  );
+                }
+              },
+            ),
           ),
         ],
       ),
